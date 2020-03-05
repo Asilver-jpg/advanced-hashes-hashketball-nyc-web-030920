04d7827cc255078d4ed808a56522c54ed07b5108
Alexander Silver <asilver7493@gmail.com>
2020-03-05 20:35:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -41,12 +41,12 @@ end
 
 def make_away()
   arr=[]
-  p1 = make_player()
-  p2= 
-  p3= 
-  p4=
-  p5=
-  
+  p1 = make_player("Jeff Adrien", 4,18,10,1,1,2,7,2)
+  p2= make_player("Bismack Biyombo", 0 ,16,12,4,7,22,15,10)
+  p3= make_player("DeSagna Diop",2,14,24,12,12,4,5,5)
+  p4=make_player("Ben Gordon",8,15,33,3,2,1,1,0)
+  p5= make_player("Kemba Walker",33,15,6,12,12,7,5,12)
+  arr.push(p1)
   
 end
 
