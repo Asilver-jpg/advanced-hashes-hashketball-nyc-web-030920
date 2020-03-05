eaa92de31602970406ec3381176e30e15a1a683b
Alexander Silver <asilver7493@gmail.com>
2020-03-05 20:26:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,23 @@
+
+
+
+
+
+def make_team (name, colors, players)
+
+
+end
 def make_player (name, number, shoe, points, rebounds, assists, steals, blocks, slam_dunks)
-  player_hash= {}
-  
+  player_hash= {:player_name => name,
+:number=> number,
+:shoe => shoe,
+:points => points,
+:rebounds => rebounds,
+:assists => assists,
+:steals => steals,
+:blocks => blocks
+:slam_dunks => slam_dunks}
+  player_hash
 end
 
 
