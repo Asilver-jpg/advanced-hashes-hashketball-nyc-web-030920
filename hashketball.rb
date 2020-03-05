678ab458bf610bb19144ee4e1bc47570874b22f3
Alexander Silver <asilver7493@gmail.com>
2020-03-05 20:33:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,30 @@
+def game_hash()
+  
+  game = {
+    :home =>{},
+    :away =>{}
+    
+  }
+  
+  
+end
 
 
-
-
+def make_colors(*args)
+  color_arr= []
+  args.each {|color| color_arr.push(color)}
+  color_arr
+end
+end
 
 def make_team (name, colors, players)
-
-
+  
+team_hash = { 
+  :name => name,
+  :colors => colors,
+  :players => players
+}
+team_hash
 end
 def make_player (name, number, shoe, points, rebounds, assists, steals, blocks, slam_dunks)
   player_hash= {:player_name => name,
@@ -20,12 +39,22 @@ def make_player (name, number, shoe, points, rebounds, assists, steals, blocks, 
   player_hash
 end
 
+def make_away()
+  arr=[]
+  p1 = make_player()
+  p2= 
+  p3= 
+  p4=
+  p5=
+  
+  
+end
 
-
-
-
-
-
-
-
-
+def make_home()
+  arr=[]
+  p1 = 
+  p2= 
+  p3= 
+  p4=
+  p5=
+end
